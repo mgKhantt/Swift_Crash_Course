bea67e84249c8b324f8977df3d41e4c32544072c
mgKhantt <khantphonenaing156@gamil.com>
2024-11-07 20:52:54 +0630

    Structure Finished

--- Structure.playground/Contents.swift
+++ Structure.playground/Contents.swift
@@ -1,3 +1,53 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+struct Person {
+    let name: String
+    let age: Int
+}
+
+let person1 = Person(name: "Kyaw Kyaw", age: 20)
+
+person1.name
+
+struct Person2 {
+    let firstName: String
+    let lastName: String
+    let role: String
+    
+    //can be use a variable as a init by using {}
+    var fullDocs: String {
+        "\(firstName) \(lastName) is doing as a \(role)"
+    }
+}
+
+let aungGyi = Person2(firstName: "Aung", lastName: "Gyi", role: "Swift Developer")
+aungGyi.fullDocs
+
+///
+///can be use a variable as a init by using {}
+let first: String = "Hello"
+let last: String = "World"
+
+var test: String {
+    "\(first) \(last)"
+}
+
+test
+///
+struct Car {
+    var currentSpeed: Int
+    mutating func drive(speed: Int) {
+        "Driving"
+        currentSpeed = speed
+    }
+}
+
+//difference between let and var for struct
+let immutableCar = Car(currentSpeed: 20)
+immutableCar.currentSpeed
+
+var mutalbleCar = Car(currentSpeed: 10)
+mutalbleCar.drive(speed: 30)
+mutalbleCar.currentSpeed
+
+
