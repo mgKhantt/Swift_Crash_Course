4fecc32a3550df2b5159992d81f2df314f9d8535
mgKhantt <khantphonenaing156@gmail.com>
2024-11-19 20:44:43 +0630

    Map

--- Collections.playground/Contents.swift
+++ Collections.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let numbers = [1, 2, 3, 4]
+numbers.first
+numbers.last
+numbers.count
+numbers.map(-)
+
+for value in numbers {
+    value
+}
+
+for value in numbers where value % 2 == 0 {
+    value
+}
+
+let mapArray = numbers.map { (value: Int) -> Int in
+    value * 2
+}
+
+mapArray
+numbers
