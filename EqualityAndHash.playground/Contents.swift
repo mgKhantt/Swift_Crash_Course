c586f169e3bc3d3d7551fd81980941d9ed6071db
mgKhantt <khantphonenaing156@gmail.com>
2024-11-19 21:07:15 +0630

    Equality and Hash

--- EqualityAndHash.playground/Contents.swift
+++ EqualityAndHash.playground/Contents.swift
@@ -1,2 +1,24 @@
 import Foundation
 
+struct Person: Equatable {
+    let id: String
+    let name: String
+}
+
+let aungaung = Person(id: "1", name: "Aung Aung")
+let kyawkyaw = Person(id: "1", name: "Kyaw Kyaw")
+
+if aungaung == kyawkyaw {
+    "They are equal"
+} else {
+    "They are not equal"
+}
+
+extension Person {
+    static func == (
+        lhs: Self,
+        rhs: Self
+    ) -> Bool {
+        lhs.id == rhs.id
+    }
+}
